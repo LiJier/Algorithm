a27f9ae93821eb462e3de9faaadb23284c21decd
lijie <lijie37927@outlook.com>
2022-06-02 23:39:21 +0800

    代码格式化

--- src/algorithm/App.java
+++ src/algorithm/App.java
@@ -4,20 +4,19 @@ public class App {
         test();
     }
 
-
-    public static void test(){
+    public static void test() {
         int a = 101;
         int b = 111;
-        System.out.printf("a^b = %d\n",a^b);
-        a = a^b;
-        System.out.printf("a = %d ",a);
-        System.out.printf("b = %d\n",b);
-        b = a^b;
-        System.out.printf("a = %d ",a);
-        System.out.printf("b = %d\n",b);
-        a = a^b;
-        System.out.printf("a = %d ",a);
-        System.out.printf("b = %d\n",b);
+        System.out.printf("a^b = %d\n", a ^ b);
+        a = a ^ b;
+        System.out.printf("a = %d ", a);
+        System.out.printf("b = %d\n", b);
+        b = a ^ b;
+        System.out.printf("a = %d ", a);
+        System.out.printf("b = %d\n", b);
+        a = a ^ b;
+        System.out.printf("a = %d ", a);
+        System.out.printf("b = %d\n", b);
     }
 
-}   
+}
--- src/algorithm/Utils.java
+++ src/algorithm/Utils.java
@@ -1,4 +1,5 @@
 package algorithm;
+
 import java.util.Arrays;
 import java.util.Random;
 
