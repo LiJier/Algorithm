8d2f4402338001fde872c6dcab9ebd6f0000d277
lijie <lijie37927@outlook.com>
2022-06-17 17:35:34 +0800

    LeetCode

--- src/leetcode/LeetCode101.java
+++ src/leetcode/LeetCode101.java
@@ -9,7 +9,17 @@ import algorithm.TreeNode;
 public class LeetCode101 {
 
     public static boolean isSymmetric(TreeNode root) {
-        return false;
+        return isMirror(root, root);
+    }
+
+    public static boolean isMirror(TreeNode h1, TreeNode h2) {
+        if (h1 == null ^ h2 == null) {
+            return false;
+        }
+        if (h1 == null && h2 == null) {
+            return true;
+        }
+        return h1.val == h2.val && isMirror(h1.left, h2.right) && isMirror(h1.right, h2.left);
     }
 
 }
