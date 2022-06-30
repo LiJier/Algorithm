c602f29b52285bdaac3f68d1a6743a744e4f9a5c
lijie <lijie37927@outlook.com>
2022-06-30 16:28:52 +0800

    LeetCode29

--- src/leetcode/LeetCode29.java
+++ src/leetcode/LeetCode29.java
@@ -11,4 +11,27 @@ package leetcode;
  */
 public class LeetCode29 {
 
+    public static int divide(int dividend, int divisor) {
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+        int sign = -1;
+        if ((dividend > 0 && divisor > 0) || (dividend < 0 && divisor < 0)) {
+            sign = 1;
+        }
+        dividend = dividend > 0 ? -dividend : dividend;
+        divisor = divisor > 0 ? -divisor : divisor;
+        int ans = 0;
+        while (dividend <= divisor) {
+            int tmp = divisor, count = 1;
+            while (tmp >= dividend - tmp) {
+                tmp += tmp;
+                count += count;
+            }
+            dividend -= tmp;
+            ans += count;
+        }
+        return sign < 0 ? -ans : ans;
+    }
+
 }
